5ae0bc5829a370bdd0114a6cccd839bec5299959
Cédric Bohnert <cbohnert.datascience@gmail.com>
2024-02-28 19:11:32 +0100

    tools/crud-tool-app.js added.

--- README.md
+++ README.md
@@ -13,27 +13,26 @@ Le stack technique de ce projet sera le trio HTML, CSS et JS. Pas d'utilisation 
 
 L'objectif de cette phase est de créer le MVP d'une application CRUD qui permettra de créer le fichier de données (au format JSON) utilisé dans la seconde phase du projet.
 
-### User Stories :
+### User Stories
 
 Les user stories suivantes couvrent toutes les fonctionnalités de base d'une application CRUD : la création (Create), la lecture (Read), la mise à jour (Update) et la suppression (Delete) de données.
 
 1. En tant qu'utilisateur, je veux pouvoir voir toutes les données stockées afin de comprendre rapidement l'ensemble des informations disponibles.
 
-    * L'application affiche un tableau avec toutes les données stockées à chaque chargement de la page.
+    - L'application affiche un tableau avec toutes les données stockées à chaque chargement de la page.
 
 2. En tant qu'utilisateur, je veux pouvoir ajouter de nouvelles données afin de compléter l'ensemble des informations.
 
-    * L'application fournit un formulaire pour entrer de nouvelles données.
-    * Lorsque l'utilisateur soumet le formulaire, les nouvelles données sont ajoutées à la fin du tableau et stockées de manière persistante.
+    - L'application fournit un formulaire pour entrer de nouvelles données.
+    - Lorsque l'utilisateur soumet le formulaire, les nouvelles données sont ajoutées à la fin du tableau et stockées de manière persistante.
 
 3. En tant qu'utilisateur, je veux pouvoir modifier les données existantes afin de corriger les erreurs ou de mettre à jour les informations.
 
-    * L'application fournit un bouton "Modifier" pour chaque ligne de données.
-    * Lorsque l'utilisateur clique sur le bouton "Modifier", les données correspondantes sont chargées dans le formulaire pour modification.
-    * Lorsque l'utilisateur soumet le formulaire, les données modifiées remplacent les anciennes données dans le tableau et sont stockées de manière persistante.
+    - L'application fournit un bouton "Modifier" pour chaque ligne de données.
+    - Lorsque l'utilisateur clique sur le bouton "Modifier", les données correspondantes sont chargées dans le formulaire pour modification.
+    - Lorsque l'utilisateur soumet le formulaire, les données modifiées remplacent les anciennes données dans le tableau et sont stockées de manière persistante.
 
 4. En tant qu'utilisateur, je veux pouvoir supprimer les données existantes afin de garder l'ensemble des informations pertinentes et à jour.
 
-    * L'application fournit un bouton "Supprimer" pour chaque ligne de données.
-    * Lorsque l'utilisateur clique sur le bouton "Supprimer", les données correspondantes sont supprimées du tableau et de manière persistante.
-
+    - L'application fournit un bouton "Supprimer" pour chaque ligne de données.
+    - Lorsque l'utilisateur clique sur le bouton "Supprimer", les données correspondantes sont supprimées du tableau et de manière persistante.
--- TOOLS/crud-tool-app.js
+++ TOOLS/crud-tool-app.js
@@ -1,18 +1,27 @@
 document.getElementById('myForm').addEventListener('submit', saveData);
 
+/**
+ * Récupère les données depuis le stockage local et les affiche dans un tableau HTML.
+ */
 function fetchData() {
     let dataArray = JSON.parse(localStorage.getItem('data'));
     let dataView = document.getElementById('myTable').getElementsByTagName('tbody')[0];
 
     dataView.innerHTML = '';
 
+    dataArray.sort((a, b) => a.name.localeCompare(b.name));    
+    
     for(let i = 0; i < dataArray.length; i++) {
         let name = dataArray[i].name;
+        let title = dataArray[i].title;
         let description = dataArray[i].description;
+        let url = dataArray[i].url;
 
         dataView.innerHTML += `<tr>
             <td>${name}</td>
+            <td>${title}</td>
             <td>${description}</td>
+            <td>${url}</td>
             <td>
                 <a href="#" onclick="deleteData('${name}')">Delete</a> | 
                 <a href="#" onclick="editData('${name}')">Edit</a>
@@ -21,14 +30,22 @@ function fetchData() {
     }
 }
 
+/**
+ * Saves the data entered in the form to the local storage.
+ * @param {Event} e - The event object.
+ */
 function saveData(e) {
     e.preventDefault();
     let name = document.getElementById('name').value;
+    let title = document.getElementById('title').value;
     let description = document.getElementById('description').value;
+    let url = document.getElementById('url').value;
 
     let data = {
         name,
-        description
+        title,
+        description,
+        url
     };
 
     if(localStorage.getItem('data') === null) {
@@ -37,6 +54,10 @@ function saveData(e) {
         localStorage.setItem('data', JSON.stringify(dataArray));
     } else {
         let dataArray = JSON.parse(localStorage.getItem('data'));
+        if (dataArray.find(item => item.name === name)) {
+            dataArray = dataArray.filter(item => item.name !== name);
+        }
+
         dataArray.push(data);
         localStorage.setItem('data', JSON.stringify(dataArray));
     }
@@ -47,6 +68,10 @@ function saveData(e) {
 }
 
 
+/**
+ * Deletes data from local storage based on the provided name.
+ * @param {string} name - The name of the item to be deleted.
+ */
 function deleteData(name) {
     if (confirm("Confirmez-vous la suppression de cet item ?")) {
         let dataArray = JSON.parse(localStorage.getItem('data'));
@@ -63,32 +88,18 @@ function deleteData(name) {
     }
 }
 
-function updateData(index) {
-    let dataArray = JSON.parse(localStorage.getItem('data'));
-    let name = document.getElementById('name').value;
-    let description = document.getElementById('description').value;
-    console.log(name);
-    console.log(dataArray[index]);
-    dataArray[index].name = name;
-    dataArray[index].description = description;
-    console.log(dataArray[index]);
-    localStorage.setItem('data', JSON.stringify(dataArray));
-
-    document.getElementById('myForm').reset();
-
-    fetchData();
-}
-
+/**
+ * Edits the data with the specified name.
+ * @param {string} name - The name of the data to be edited.
+ */
 function editData(name) {
     let dataArray = JSON.parse(localStorage.getItem('data'));
     for(let i = 0; i < dataArray.length; i++) {
         if(dataArray[i].name == name) {
             document.getElementById('name').value = dataArray[i].name;
+            document.getElementById('title').value = dataArray[i].title;
             document.getElementById('description').value = dataArray[i].description;
-            document.getElementById('myForm').onsubmit = function(e) {
-                e.preventDefault();
-                updateData(i);
-            }
+            document.getElementById('url').value = dataArray[i].url;
         }
     }
 }
